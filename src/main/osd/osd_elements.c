7a7a46896521ef5519de319d22b1d0f50a4aede5
MatajZ <mataj.zandona@orqafpv.com>
2025-04-22 22:11:57 +0200

    change the osd element to classic element variant

--- src/main/osd/osd_elements.c
+++ src/main/osd/osd_elements.c
@@ -1550,6 +1550,12 @@ static void osdElementVtxChannel(osdElementParms_t *element)
     const char vtxBandLetter = vtxCommonLookupBandLetter(vtxDevice, vtxSettingsConfig()->band);
     const char *vtxChannelName = vtxCommonLookupChannelName(vtxDevice, vtxSettingsConfig()->channel);
     unsigned vtxStatus = 0;
+
+    uint16_t power = getVtxPower();
+    VTX_BAND band = getVtxBand();
+    uint8_t channel = getVtxChan();
+    //uint16_t frequency = getVtxFreq();
+
     uint8_t vtxPower = vtxSettingsConfig()->power;
     if (vtxDevice) {
         vtxCommonGetStatus(vtxDevice, &vtxStatus);
@@ -1572,6 +1578,26 @@ switch (element->type) {
             tfp_sprintf(element->buff, "%s", vtxPowerLabel);
         break;
 
+    case OSD_ELEMENT_TYPE_3:
+        if(power > 999)
+        {
+            int power_int = power / 1000;
+            int power_frac = ((power - (power_int * 1000)) / 100);
+            if(power_frac != 0)
+                tfp_sprintf(element->buff, "%s:%d:%d.%dW",  vtx_band_names[band], channel, power_int, power_frac);
+                //tfp_sprintf(element->buff, "%d:%d.%dW",  frequency, power_int, power_frac);
+            else
+                tfp_sprintf(element->buff, "%s:%d:%dW",  vtx_band_names[band], channel, power_int);
+                //tfp_sprintf(element->buff, "%d:%dW",  frequency, power_int);
+
+        }
+        else
+        {
+            tfp_sprintf(element->buff, "%s:%d:%d",  vtx_band_names[band], channel, power);
+            //tfp_sprintf(element->buff, "%d:%d",  frequency, power);
+        }
+        break;
+
     default:
         if (vtxStatus & VTX_STATUS_LOCKED) {
             tfp_sprintf(element->buff, "-:-:-:L");
@@ -1588,7 +1614,9 @@ switch (element->type) {
 #ifdef USE_RX_VTX_HYBRID
 static void osdElementVtxPower(osdElementParms_t *element)
 {
-    uint16_t power = getVtxPower();
+
+    tfp_sprintf(element->buff, "hello");
+    /*uint16_t power = getVtxPower();
     //VTX_BAND band = getVtxBand();
     //uint8_t channel = getVtxChan();
     uint16_t frequency = getVtxFreq();
@@ -1607,7 +1635,7 @@ static void osdElementVtxPower(osdElementParms_t *element)
     else
     {
         tfp_sprintf(element->buff, "%d:%d",  frequency, power);
-    }
+    }*/
 }
 #endif
 
@@ -1685,7 +1713,6 @@ static const uint8_t osdElementDisplayOrder[] = {
     OSD_FLYMODE,
     OSD_THROTTLE_POS,
     OSD_VTX_CHANNEL,
-    OSD_VTX_POWER,
     OSD_CURRENT_DRAW,
     OSD_MAH_DRAWN,
     OSD_WATT_HOURS_DRAWN,
@@ -1767,6 +1794,7 @@ static const uint8_t osdElementDisplayOrder[] = {
     OSD_SYS_WARNINGS,
     OSD_SYS_VTX_TEMP,
     OSD_SYS_FAN_SPEED,
+    OSD_VTX_POWER,
 };
 
 // Define the mapping between the OSD element id and the function to draw it
@@ -1790,7 +1818,7 @@ const osdElementDrawFn osdElementDrawFunction[OSD_ITEM_COUNT] = {
     [OSD_VTX_CHANNEL]             = osdElementVtxChannel,
 #endif
 #ifdef USE_RX_VTX_HYBRID
-    [OSD_VTX_POWER]             = osdElementVtxPower,
+    [OSD_VTX_POWER]               = osdElementVtxPower,
 #endif
     [OSD_CURRENT_DRAW]            = osdElementCurrentDraw,
     [OSD_MAH_DRAWN]               = osdElementMahDrawn,
